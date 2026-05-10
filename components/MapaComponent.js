53d42c473bdfeea562f4f6c401a64d4c2a5a993a
Laila <azizi.122844@e.unavarra.es>
2026-05-10 21:42:10 +0200

    Añadir json de caminos y adaptar componente Mapa para mostrar la ruta

--- components/MapaComponent.js
+++ components/MapaComponent.js
@@ -1,12 +1,43 @@
 import { Component } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import { View, Text, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Polyline, Marker } from "react-native-maps";
+import { View, StyleSheet, Dimensions } from "react-native";
+import caminoData from "../data_provisional/caminoFrances.json";
 
 class Mapa extends Component {
+  constructor(props) {
+    super(props);
+    const rawCoords = caminoData.features[0].geometry.coordinates;
+    const formattedCoords = rawCoords.map((punto) => ({
+      latitude: punto[1],
+      longitude: punto[0],
+    }));
+
+    this.state = {
+      routeCoords: formattedCoords,
+    };
+  }
+
   render() {
+    const { routeCoords } = this.state;
+
     return (
       <View style={styles.container}>
-        <MapView provider={PROVIDER_GOOGLE} style={styles.map} />
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: 42.466,
+            longitude: -2.445,
+            latitudeDelta: 5,
+            longitudeDelta: 5,
+          }}
+        >
+          <Polyline
+            coordinates={routeCoords}
+            strokeColor="#005293"
+            strokeWidth={4}
+          />
+        </MapView>
       </View>
     );
   }
@@ -17,8 +48,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: "100%",
-    height: "100%",
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
 
