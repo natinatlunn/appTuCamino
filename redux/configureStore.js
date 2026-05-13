fa1bb1fc0322f75a5bb750171717fc2b1345d97c
natinatlunn <127200417+natinatlunn@users.noreply.github.com>
2026-05-13 18:09:28 +0200

    Creación carpetas para redux y codigo en configureStore

---
+++ redux/configureStore.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { rutasReducer } from './reducers/rutasReducer';
+import { userReducer } from './reducers/userReducer';
+import { comentariosReducer } from './reducers/comentariosReducer';
+import { scannerReducer } from './reducers/scannerReducer';
+
+export const ConfigureStore = () => {
+    const store = configureStore({
+        reducer: {
+            rutas: rutasReducer,
+            user: userReducer,
+            comentarios: comentariosReducer,
+            scanner: scannerReducer,
+        },
+    });
+
+    return store;
+};
